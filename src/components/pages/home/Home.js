e2354a856bc199700c89d064e41b2dd40a70f0ee
Filipe Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2023-11-19 18:28:41 -0300

    atualização no projeto

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -10,6 +10,7 @@ import { FaInstagram, FaLinkedin } from "react-icons/fa";
 
 import Navbar from "../../navbar/Navbar";
 import Botao from "../../botao/Botao";
+import PlanoCasamento from "../../planoCasamento/PlanoCasamento";
 
 export default function Home() {
   return (
@@ -59,53 +60,20 @@ export default function Home() {
       </section>
       <section className="caminho-planejamento">
         <div className="caixa-textos">
-          <div className="texto">
-            <div className="conteudo-home">
-              <h2>
-                1.
-                <br />
-                Seu plano ideal
-              </h2>
-              <p>
-                Oferecemos um plano personalizado para tornar o seu casamento
-                ideal uma realidade. Nossa abordagem é feita sob medida, levando
-                em consideração suas preferências individuais.
-              </p>
-              <Botao to={'/sobrenos'} texto={"Saiba mais"} />
-            </div>
-            <img src={ImagemTexto1} alt="" />
-          </div>
+          <PlanoCasamento numero={"1."} titulo={"Seu plano ideal"} imagemTexto={ImagemTexto1} texto={"Oferecemos um plano personalizado para tornar o seu casamento ideal uma realidade. Nossa abordagem é feita sob medida, levando em consideração suas preferências individuais."} />
+
           <div className="texto">
             <img src={ImagemTexto2} alt="" />
             <div className="conteudo-home">
-              <h2>
-                2.
-                <br />
-                Alinhamento de ideias
-              </h2>
-              <p>
-                Na etapa de alinhamento, estabelecemos contato para moldar,
+              <h2>2.<br />Alinhamento de ideias</h2>
+              <p>Na etapa de alinhamento, estabelecemos contato para moldar,
                 criar e alinhar suas ideias. Juntos, trabalhamos para encontrar
                 o local perfeito que corresponda aos seus desejos e faça o seu
-                casamento ser verdadeiramente especial.
-              </p>
+                casamento ser verdadeiramente especial.</p>
             </div>
           </div>
-          <div className="texto">
-            <div className="conteudo-home">
-              <h2>
-                3.
-                <br />O dia do SIM!
-              </h2>
-              <p>
-                Chegamos à etapa do casamento, tudo está finalizado. Agora é só
-                desfrutar ao máximo e confiar em nossa expertise para garantir
-                que tudo ocorra de forma perfeita e conforme planejado.
-              </p>
-              <Botao to={"/"} texto={"Eu quero!"} />
-            </div>
-            <img src={ImagemTexto3} alt="" />
-          </div>
+
+          <PlanoCasamento numero={"3."} titulo={"O dia do SIM!"} texto={"Chegamos à etapa do casamento, tudo está finalizado. Agora é só desfrutar ao máximo e confiar em nossa expertise para garantir que tudo ocorra de forma perfeita e conforme planejado."} imagemTexto={ImagemTexto3}/>
         </div>
       </section>
     </>
