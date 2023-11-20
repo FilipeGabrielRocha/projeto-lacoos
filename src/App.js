33da3745e9f759595909f8bd33a00c1426a8b7a4
Filipe Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2023-11-19 22:19:48 -0300

    Criação tela pagamento

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './components/pages/login/Login';
 import RecuperacaoSenha from './components/pages/recuperacaoSenha/RecuperacaoSenha';
 import NovaConta from './components/pages/novaConta/NovaConta';
 import Perfil from './components/pages/perfil/Perfil';
+import Pagamento from './components/pages/pagamento/Pagamento';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/recuperacao-senha" element={<RecuperacaoSenha />} />
         <Route exact path="/nova-conta" element={<NovaConta />} />
         <Route exact path="/perfil" element={<Perfil />} />
+        <Route exact path="/pagamento" element={<Pagamento />} />
       </Routes>
     </BrowserRouter>
   );
